c53f1a14e826569f5d21866e032711fe02a4ec39
yuanfang <65866693@qq.com>
2020-10-26 22:28:54 +0800

    修改home.vue，模拟数据列表

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,67 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+<div class="home">
+    <van-nav-bar title="首页" right-text="下拉刷新" fixed class="navbar" />
+    <van-pull-refresh v-model="refreshing" @refresh="onRefresh">
+        <van-list v-model:loading="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
+            <van-cell v-for="item in list" :key="item.id" :title="item.title" />
+        </van-list>
+    </van-pull-refresh>
+</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
+    data() {
+        return {
+            list: [],
+            loading: false,
+            finished: false,
+            refreshing: false
+        };
+    },
+    methods: {
+        onLoad() {
+            setTimeout(() => {
+                if (this.refreshing) {
+                    this.list = [];
+                    this.refreshing = false;
+                }
+
+                for (let i = 0; i < 10; i++) {
+                    let id = this.list.length + 1;
+                    let title = "Hello:" + id;
+                    this.list.push({
+                        id: id,
+                        title: title
+                    });
+                }
+                this.loading = false;
+
+                if (this.list.length >= 40) {
+                    this.finished = true;
+                }
+            }, 1000);
+        },
+        onRefresh() {
+            // 清空列表数据
+            this.finished = false;
+
+            // 重新加载数据
+            // 将 loading 设置为 true，表示处于加载状态
+            this.loading = true;
+            this.onLoad();
+        }
+    }
 };
 </script>
+
+<style scoped>
+.home {
+    background-color: #f8f8f8;
+}
+
+.navbar {
+    background-color: #f8f8f8;
+    color: black;
+}
+</style>
